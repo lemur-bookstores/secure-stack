d286c082eb31889e18f1437812b29f02b9519dda
Elkin Portocarrero <elkincp5@gmail.com>
2025-12-05 14:14:48 -0500

    feat (framework  secure-stack): Definition of a framework-type package for the complete installation of the tool

---
+++ packages/secure-stack/src/index.ts
@@ -0,0 +1,25 @@
+import * as Audit from '@lemur-bookstores/audit';
+import * as Auth from '@lemur-bookstores/auth';
+import * as Cache from '@lemur-bookstores/cache';
+import * as Client from '@lemur-bookstores/client';
+import * as Core from '@lemur-bookstores/core';
+import * as Mesh from '@lemur-bookstores/mesh';
+import * as RateLimit from '@lemur-bookstores/rate-limit';
+import * as Rbac from '@lemur-bookstores/rbac';
+import * as Realtime from '@lemur-bookstores/realtime';
+import * as Server from '@lemur-bookstores/server';
+import * as Storage from '@lemur-bookstores/storage';
+
+export {
+    Audit,
+    Auth,
+    Cache,
+    Client,
+    Core,
+    Mesh,
+    RateLimit,
+    Rbac,
+    Realtime,
+    Server,
+    Storage
+};
